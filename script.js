2165d138526d82d785bea489114f8c557ab24de7
Oles Odynets <olchyk89@gmail.com>
2019-02-05 18:46:27 +0100

    Model

---
+++ script.js
@@ -0,0 +1,15 @@
+const entities = {
+
+}
+
+function preload() {
+	// Load your models here...
+}
+
+function setup() {
+	createCanvas(innerWidth - .5, innerHeight - .5);
+}
+
+function draw() {
+	background('white');
+}
